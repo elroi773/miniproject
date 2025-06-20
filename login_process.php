70bec00ed8abfdc18d01559274fab8c47099f348
elroi773 <s2457@e-mirim.hs.kr>
2025-06-20 14:51:52 +0900

    login

--- login_process.php
+++ login_process.php
@@ -6,4 +6,9 @@ require_once 'db.php';
 $userid = trim($_POST['userid'] ?? '');
 $password = $_POST['password'] ?? '';
 
+if(!$userid || !$password){
+  echo"<script>alert('다 입력해주세용~!'); history.back();</script>";
+  exit;
+}
+
 ?>
