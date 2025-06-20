f63631c09836c4f98f3db1054520d42783f01203
elroi773 <s2457@e-mirim.hs.kr>
2025-06-20 15:00:46 +0900

    로그인 끗

--- login_process.php
+++ login_process.php
@@ -11,4 +11,25 @@ if(!$userid || !$password){
   exit;
 }
 
+$stmt = $pdo->prepare("SELECT * FROM users WHERE userid=?"); // 해킹 방지 
+$stmt->execute([$userid]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if(!$user){ 
+  echo"<script>alert('가입 하셨조..?'); history.back();</script>";
+  exit;
+}
+
+if(!password_verify($password, $user['password'])){
+  echo"<script>alert('비번 틀림'); history.back();</script>";
+  exit;
+}
+
+
+$_SESSION['user_id'] = $user['userid'];
+$_SESSION['id'] = $user['id'];
+
+
+header('Location:board.php');
+exit;
 ?>
